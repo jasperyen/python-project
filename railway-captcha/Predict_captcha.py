79da32850136cac11850c12b3728a7aec9b5e1fa
JasperYen <jasper1996826@gmail.com>
2019-03-05 02:30:07 +0800

    update env

--- railway-captcha/Predict_captcha.py
+++ railway-captcha/Predict_captcha.py
@@ -12,10 +12,8 @@ captcha_width = 128
 letter_str = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_'
 letter_amount = len(letter_str)
 
-model_path = '../../model/captcha_model.h5'
-dir_path = '../../data/captcha/'
-
-#dir_path = 'C:\\workspace\\temp\\test_data\\wrong\\'
+model_path = 'captcha_model.h5'
+dir_path = 'data/'
 
 print('Loading model : ' + model_path)
 model = load_model(model_path)
